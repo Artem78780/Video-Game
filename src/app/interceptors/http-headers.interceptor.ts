21c7ba6187082c0e815c4e61f70f5bac994747a9
Artem78780 <parashhukartem@gmail.com>
2023-03-28 20:05:08 +0300

    finish create interceptors

---
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -0,0 +1,26 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+ 
+@Injectable()
+
+export class HttpHeadersInterceptor implements HttpInterceptor {
+    constructor() {}
+
+    intercept(
+        req: HttpRequest<any>,
+        next: HttpHandler
+    ): Observable<HttpEvent<any>> {
+        req = req.clone({
+            setHeaders: {
+                'X-RapidAPI-Key': 'dfed6e196amsh64f5168fab207b3p1ac245jsnd24c23df5c0a',
+                'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com'
+              },
+            setParams: {
+                key: '8329869cf3f74116ae0cafb1d6e411aa'
+            }
+        })
+        return next.handle(req)
+    }
+}
